6155de976c74fa89427ce532d1a1ecb4aacc5284
Connor <cafitzp1@asu.edu>
2018-10-22 23:39:20 -0700

    BinarySearch solution finished

--- Program.cs
+++ Program.cs
@@ -8,6 +8,8 @@ namespace VSConsoleApp1
         private static void Main(string[] args)
         {
             Int32[] arr = {2, 3, 4, 10, 40};
+
+            // write the index of element '10' to the console
             Console.WriteLine(BinarySearch(arr, 0, arr.Length-1, 10));
         }
 
@@ -15,17 +17,22 @@ namespace VSConsoleApp1
         {
             if(r >= l)
             {
+                // get the mid array index
                 Int32 mid = l+(r-l)/2;
 
+                // return this index if array index at mid already matches
                 if(arr[mid] == x)
                     return mid;
 
+                // recursively call binarysearch halving the lowerlimit (l) if x is greater than the midpoint
                 if(arr[mid] < x)
                     return BinarySearch(arr, mid+1, r, x);
 
+                // recursively call binarysearch halving the upper limit (r) if x is less than the midpoint
                 return BinarySearch(arr, l, mid-1, x);
             }
 
+            // element was not in the array if we reach this point
             return -1;
         }
     }
