7493269f4a9f1b419030a9dbb2aac4c46a42abc0
Connor <cafitzp1@asu.edu>
2018-10-22 23:58:33 -0700

    Master branch is now a cleared canvas, other practice solutions have been placed in their own branches

--- Program.cs
+++ Program.cs
@@ -7,45 +7,7 @@ namespace VSConsoleApp1
     {
         private static void Main(string[] args) 
         {
-            int[][] jaggedArray = new int [][]
-            {
-                new int[] { 0, 1, 1, 2 },
-                new int[] { 0, 5, 0, 0 }, 
-                new int[] { 2, 0, 3, 3 } 
-            };   
-
-            int test = MatrixElementSum(jaggedArray);
-
-            Console.WriteLine($"Test val: {test}");
+            
         } 
-
-        public static int MatrixElementSum(int[][] matrix)
-        {
-            Int32 runningSum = 0;
-
-            // iterate each matrix column
-            for(int i = 0; i<matrix[0].Length; i++)
-            {
-                // iterate each row
-                for (int j = 0; j<matrix.Length; j++)
-                {
-                    // grab the element of array j, item i (aka column j, row i)
-                    Int32 currentElement = matrix[j][i];
-
-                    if(currentElement <= 0)
-                    {
-                        // exit loop for this column if a val is 0 or less
-                        break;
-                    }
-                    else
-                    {
-                        // add val to running sum
-                        runningSum += currentElement;
-                    }
-                }
-            }
-
-            return runningSum;
-        }
     }
 }
